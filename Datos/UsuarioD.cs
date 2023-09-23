17457c0ee4cbe744de8934381320396f3ea345fe
pscalello <pscalello@hotmail.com>
2023-09-22 21:27:34 -0300

    Datos

---
+++ Datos/UsuarioD.cs
@@ -0,0 +1,84 @@
+﻿using Datos.Utilidades;
+using Entidad;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Net.Http;
+
+
+namespace Datos
+{
+    public class UsuarioD
+    {
+        public static void CrearUsuario(UsuarioE usuario)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(usuario);
+
+            HttpResponseMessage response = WebHelper.Post("Usuario/AgregarUsuario", jsonRequest);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(response.StatusCode.ToString());
+            }
+        }
+
+        //public static string Login(Login login)
+        //{
+        //    var jsonRequest = JsonConvert.SerializeObject(login);
+
+        //    HttpResponseMessage response = WebHelper.Post("Usuario/Login", jsonRequest);
+
+        //    if (!response.IsSuccessStatusCode)
+        //    {
+        //        throw new Exception("Verifique los datos ingresados");
+        //    }
+
+        //    var reader = new StreamReader(response.Content.ReadAsStream());
+
+        //    String respuesta = reader.ReadToEnd();
+
+        //    return respuesta;
+        //}
+
+        public static string CambiarContraseña(string nombreUsuario, string contraseña, string contraseñaNueva)
+        {
+            Dictionary<String, String> map = new Dictionary<String, String>();
+            map.Add("nombreUsuario", nombreUsuario);
+            map.Add("contraseña", contraseña);
+            map.Add("contraseñaNueva", contraseñaNueva);
+
+            var jsonRequest = JsonConvert.SerializeObject(map);
+
+            HttpResponseMessage response = WebHelper.Patch("Usuario/CambiarContraseña", jsonRequest);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Verifique los datos ingresados");
+            }
+
+            var reader = new StreamReader(response.Content.ReadAsStream());
+
+            String respuesta = reader.ReadToEnd();
+
+            return respuesta;
+        }
+
+        public static void BorrarUsuario(string idUsuario, string idUsuarioMaster)
+        {
+            Dictionary<String, String> map = new Dictionary<String, String>();
+            map.Add("id", idUsuario);
+            map.Add("idUsuario", idUsuarioMaster);
+
+            var jsonRequest = JsonConvert.SerializeObject(map);
+
+            HttpResponseMessage response = WebHelper.DeleteConBody("Usuario/BajaUsuario", jsonRequest);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Verifique los datos ingresados");
+            }
+
+        }
+    }
+}
---
+++ Datos/Utilidades/WebHelper.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Utilidades
+{
+    internal static class WebHelper
+    {
+        static HttpClient client = new HttpClient();
+        static string rutaBase = "https://cai-tp.azurewebsites.net/api/";
+
+        public static HttpResponseMessage Get(string url)
+        {
+            var uri = rutaBase + url;
+
+            HttpResponseMessage response = client.GetAsync(uri).Result;  // Blocking call!
+
+            return response;
+        }
+
+        public static HttpResponseMessage Post(string url, String jsonRequest)
+        {
+            var uri = rutaBase + url;
+
+            var data = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PostAsync(uri, data).Result;
+
+            return response;
+
+        }
+
+        public static HttpResponseMessage Put(string url, String jsonRequest)
+        {
+            var uri = rutaBase + url;
+
+            var data = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PutAsync(uri, data).Result;
+
+            return response;
+
+        }
+
+        public static HttpResponseMessage Patch(string url, String jsonRequest)
+        {
+            var uri = rutaBase + url;
+
+            var data = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = client.PatchAsync(uri, data).Result;
+
+            return response;
+
+        }
+
+        public static HttpResponseMessage DeleteConBody(string url, String jsonRequest)
+        {
+            var uri = rutaBase + url;
+
+            HttpRequestMessage request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri(uri),
+                Content = new StringContent(jsonRequest, Encoding.UTF8, "application/json")
+            };
+            HttpResponseMessage response = client.SendAsync(request).Result;
+
+            return response;
+        }
+    }
+}
